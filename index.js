eb6cb2d882d32a7d8e56a36dbf1a15bf982a05d6
Ankit Shah <ankit@fa.biz>
2018-12-11 14:29:10 -0500

    readme added

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const func = require('./commonFunctions')
 
-func.checkAPIStatus('http://fabexplorer.com')
+
+async function test() {
+    let apiStatus = await func.checkAPIStatus('http://fabexplorer.info')
+    console.log("API Status : "+apiStatus)
+    if(!apiStatus) {
+        console.log("The API is currently down. Please try again after some time.")
+        return
+    }
+
+    //This function can be used to check the balance with only mnemonics available
+    let balance = await func.getBalanceForMnemonics('zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo wrong')
+    console.log("Balance : "+balance)
+}
+
+
+test()
+
